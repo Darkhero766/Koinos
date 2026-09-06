d36aeb7b5d6bb2e211d3b55058cf087b07525784
Faizal <nightowlclub72@gmail.com>
2026-09-06 16:42:09 +0530

    Load KOINOS UI fixes

--- site-config.js
+++ site-config.js
@@ -1,10 +1,3 @@
 /* KOINOS browser configuration. No secrets belong here. */
-window.KOINOS_CONFIG = {
-  // The frontend tries these in order so a Render service rename does not break the site.
-  apiCandidates: [
-    window.KOINOS_API_BASE,
-    'https://koinos-api-5v03.onrender.com',
-    'https://koinos-api.onrender.com'
-  ].filter(Boolean),
-  photoBucket: 'issue-images'
-};
+window.KOINOS_CONFIG={apiCandidates:[window.KOINOS_API_BASE,'https://koinos-api-5v03.onrender.com','https://koinos-api.onrender.com'].filter(Boolean),photoBucket:'issue-images'};
+(function(){var s=document.createElement('script');s.src='./ui-fixes.js?v=20260906-12';s.defer=true;document.head.appendChild(s);})();
